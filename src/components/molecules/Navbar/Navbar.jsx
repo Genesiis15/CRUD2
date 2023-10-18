dd2ae782c72c813d10336a991465d10765822538
rojasgenesiis15@gmail.com <rojasgenesiis15@gmail.com>
2023-10-18 16:47:39 -0400

    feat(Navbar): createMolecilesNavbar
    Se creo el componente de Navbar
    Se crearon las rutas para Home, Service, About.
    En el navbar se paso la informacion de los Link y se unio el Brand con el logo.
    
    Author: Genesis Rojas

---
+++ src/components/molecules/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import {Link} from '../../atoms/Link/Link'
+import {NavbarStyle} from './styles'
+import {Brand} from '../../atoms/Brand/Brand'
+
+export const Navbar =() =>{
+    return <NavbarStyle>
+
+    <Brand img={'https://cdn-icons-png.flaticon.com/128/1400/1400491.png'} link={'/'}/>
+<div>
+<Link description={'Home'} link={'/'} color='secondary'/>
+    <Link description={'Service'} link={'/service'} color='secondary'/>
+    <Link description={'About'} link={'/about'} color='secondary'/>
+</div>
+
+    </NavbarStyle>
+}
