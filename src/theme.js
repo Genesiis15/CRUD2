a7daa0fb61be50a2b6a3bac4e712c16e9475ecdf
msanchez19 <manguilas2525@gmail.com>
2023-10-18 17:20:40 -0400

    feat(molecules): Create Footer
    Implementation of Components
    
    Author: Maikol Sanchez

--- src/theme.js
+++ src/theme.js
@@ -2,5 +2,12 @@ export const colors = {
     primary:'#192655',
     secondary:'#3876BF',
     danger: '#E1AA74',
-    info:'#F3F0CA'
+    info:'#F3F0CA',
+    white:'#fff'
 }
+
+export const getColor = (color) => {
+    return color == 'white' && colors.white || color == 'primary' && colors.primary || color == 'secondary' && colors.secondary || color == 'danger' && colors.danger || color == 'info' && colors.info  
+}
+
+export const title ='FrenchieLovers'
