f6d834436025dce8d1ca82291d2b11a913e3ca9e
msanchez19 <manguilas2525@gmail.com>
2023-10-18 14:04:39 -0400

    feat(atoms): Create Link
    Add props and styles with props in styled components
    
    Author: Maikol Sanchez

---
+++ src/components/atoms/Link/Link.jsx
@@ -0,0 +1,6 @@
+import {LinkStyle} from './styles'
+export const Link = ({link, description, color='primary'}) => {
+  return (
+    <LinkStyle to={link} color={color}>{description}</LinkStyle>
+  )
+}
---
+++ src/theme.js
@@ -0,0 +1,6 @@
+export const colors = {
+    primary:'#192655',
+    secondary:'#3876BF',
+    danger: '#E1AA74',
+    info:'#F3F0CA'
+}
