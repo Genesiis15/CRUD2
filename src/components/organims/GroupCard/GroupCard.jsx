59dd2d9bb08a454f1f39519e8bf6077aaa3cfc9e
rojasgenesiis15@gmail.com <rojasgenesiis15@gmail.com>
2023-10-20 13:04:07 -0400

    feat(title)

--- src/components/organims/GroupCard/GroupCard.jsx
+++ src/components/organims/GroupCard/GroupCard.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import { getQuery } from '../../../queries'
+import { Title } from '../../atoms/Title/Title'
 
 export const GroupCard = () => {
     const [products, setProducts] = useState([]);
@@ -9,11 +10,17 @@ export const GroupCard = () => {
     }, []);
 
     return (
-        <div style={{ marginBottom: '150px' }}>
-            {products.map(({ id, title, image }) => <div key={id}>
-                <h1>{title}</h1>
-                <img src={image} width={200} height={200} />
-            </div>)}
+
+
+        <div>
+
+            <Title title={'Frenchie'} />
+            <div style={{ marginBottom: '150px' }}>
+                {products.map(({ id, title, image }) => <div key={id}>
+                    <h1>{title}</h1>
+                    <img src={image} width={200} height={200} />
+                </div>)}
+            </div>
         </div>
     )
 }
