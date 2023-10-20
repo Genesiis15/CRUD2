7a1dac2fc03a5797d8f97c39fbbe4f8ad5bf2bc2
rojasgenesiis15@gmail.com <rojasgenesiis15@gmail.com>
2023-10-20 13:43:10 -0400

    feat(atoms): Create ImageCircle
    Props url
    
    Author: Genesis Rojas

---
+++ src/components/atoms/ImageCircle/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+export const Content = styled.div`
+    width:150px;
+    height:150px;
+    border-radius:50%;
+    border:1px solid silver; 
+    box-sizing: border-box;
+    overflow: hidden;
+`
+export const ImgStyle = styled.img`
+    object-fit: contain;
+    width:100%;
+    height:100%;
+`
