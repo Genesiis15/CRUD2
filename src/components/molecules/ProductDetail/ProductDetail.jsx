e740201d101c5f7f3819f0b2877ead40ac396e89
msanchez19 <manguilas2525@gmail.com>
2023-10-25 10:11:38 -0400

    feat(whatsapp)

--- src/components/molecules/ProductDetail/ProductDetail.jsx
+++ src/components/molecules/ProductDetail/ProductDetail.jsx
@@ -1,9 +1,13 @@
 import { Section } from '../../molecules'
 import { Description, Title } from '../../atoms'
-import { Content, PriceContent, Button } from './styles'
+import { Content } from './styles'
+import { ButtonCard } from '../../atoms'
 import { useTheme } from 'styled-components';
+import { ProductsContext } from '../../../context/productsContext'
+import { useContext } from 'react'
 export const ProductDetail = ({ data }) => {
     const theme = useTheme()
+    const { products, setProducts } = useContext(ProductsContext)
     return (
         <div style={{ display: 'flex', flexWrap: 'wrap', gap: '10px', marginTop: '20px', padding: '40px', justifyContent: 'space-around' }}>
             <Section>
@@ -12,7 +16,10 @@ export const ProductDetail = ({ data }) => {
             <Content>
                 <Title title={data.title} color={theme.primary} />
                 <Description text={data.description} color={'secondary'} />
-                <Button theme={theme.info} color={theme.white}>{`$${data.price}`} </Button>
+                <ButtonCard theme={theme.info} color={theme.info} text={`$${data.price}`} handleClick={() => {
+                    alert(`Se agrego al carrito el: ${data.title}`)
+                    setProducts([...products, data])
+                }} />
             </Content>
         </div>
     )
--- src/context/productsContext.jsx
+++ src/context/productsContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from 'react'
 export const ProductsContext = createContext({})
 
 export const ProductsProvider = ({ children }) => {
-    const [products, setProducts] = useState(['Camara'])
+    const [products, setProducts] = useState([])
     return <ProductsContext.Provider value={{ products, setProducts }}>
         {children}
     </ProductsContext.Provider>
